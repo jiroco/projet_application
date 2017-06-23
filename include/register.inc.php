9111c614fda7029dea09b47580f243c206e2bfb4
davidneyron <david.neyron@gmail.com>
2017-06-23 14:14:17 +0200

    Ajout de l'envois du mail admin

--- include/register.inc.php
+++ include/register.inc.php
@@ -9,7 +9,7 @@ if(isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['prenom']) 
     $passwd=sha1($passwd);
     $username=htmlspecialchars($_POST['username']);
 	$req = $DBcon->prepare('INSERT INTO `user`(`ID`, `NOM`, `PRENOM`, `MAIL`, `USERNAME`, `PASSWORD`) VALUES (NULL,?,?,?,?,?)');
-					
+
     $req->bindValue(1, $nom, PDO::PARAM_STR);
     $req->bindValue(2, $prenom, PDO::PARAM_STR);
     $req->bindValue(3, $email, PDO::PARAM_STR);
@@ -17,9 +17,38 @@ if(isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['prenom']) 
     $req->bindValue(5, $passwd, PDO::PARAM_STR);
 
     $check=$req->execute();
-    
+
+    /* ----------------- ENVOIS DU MAIL ------------------*/
+
+    $to = "admin@admin.com";
+    $subject = "Demande de création de compte";
+    $message = '
+    <html>
+        <head>
+            <title>Demande de création de compte</title>
+        </head>
+        <body>
+            <div>
+                <p>
+                    Un client souahite créer un compte sur le site INSA\'script
+                    <br/>
+                    <ul>
+                        <li>' . $_POST["prenom"] . '<li>
+                        <li>' . $_POST["nom"] . '<li>
+                        <li>' . $_POST["email"]. '<li>
+                        <li>' . $_POST["entreprise"] . '<li>
+    ';
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    mail('david.neyron@gmail.com','Demande de création de compte','OK', null);
+
+    echo '<p><br/>Votre demande de création de compte a bien été enregistrée et va être traitée</p>';
+
+    /*----------------------- FIN ENVOIS MAIL ---------------*/
+
     if($check){
-        echo "<meta http-equiv='refresh' content='0; URL=../login.php'>";    
+        echo "<meta http-equiv='refresh' content='0; URL=../login.php'>";
     }
     else
         echo "Un problème s'est produit lors de la requete.<br>";
@@ -28,6 +57,6 @@ if(isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['prenom']) 
 else
     include("../register.php");
     echo "</br><div class='container col-md-4 col-md-offset-4'><div class='alert alert-danger' style='text-align: center;'> <strong>Attention !</strong> un champ est vide. </div></div>";
-    
+
 $req->closeCursor();
 ?>
