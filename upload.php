8002227d10cd731e9044ca8de79358e174582f66
ClaireHmt <claire.hamonet2013@laposte.net>
2017-06-23 11:44:16 +0200

    modifié :         upload.php

--- upload.php
+++ upload.php
@@ -5,13 +5,11 @@
 </head>
 <body>
 
-<div>
+<form method="post" action="actionupload.php" enctype="multipart/form-data">
 	Sélectionner le fichier à télécharger.
 	<br><input type="file" name="file" id="File" autocomplete="on" required="required" /><br>
 	<input type="submit" name="submit" value="Upload" />
-</div>
-
-<?php include("include/connexion.inc.php");?>
+</form>
 
 </body>
 </html>
