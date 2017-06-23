4b523ca682538e4405d34d0de5674a88d67f99ac
davidneyron <david.neyron@gmail.com>
2017-06-23 17:43:54 +0200

    Creat accueil_admin

---
+++ accueil_admin.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if ((!isset($_SESSION['IDUSER'])) || (empty($_SESSION['IDUSER']))){
+    echo "<meta http-equiv='refresh' content='0; URL=index.php'>";
+}
+?>
+
+<html>
+<head>
+	<title>Projet application</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body >
+<?php include("layout_base.php"); ?>
+<div>
+	fichier uploader
+</div>
+
+<div>
+	fichier downloader
+</div>
+
+</body>
+</html>
