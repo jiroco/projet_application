906aeb7712537e843e87e6e767a05a178a64dde8
MacBookPro <macbookpro@MacBook-Pro-Aloise.local>
2017-06-23 15:28:10 +0200

    nouvelle BDD

--- projetAppli.sql
+++ projetAppli.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:8889
--- Généré le :  Ven 23 Juin 2017 à 13:16
+-- Généré le :  Ven 23 Juin 2017 à 15:26
 -- Version du serveur :  5.6.35
 -- Version de PHP :  7.0.15
 
@@ -13,80 +13,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `projetAppli`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `DEFAILLANCE`
---
-
-CREATE TABLE `DEFAILLANCE` (
-  `IDDEFAI` int(11) NOT NULL,
-  `PERTEFONCT` varchar(256) NOT NULL,
-  `INTEMP` varchar(256) NOT NULL,
-  `RETARDFONCT` varchar(256) NOT NULL,
-  `DEMIMPOSSIBLE` varchar(256) NOT NULL,
-  `ARRETIMPOSSIBLE` varchar(256) NOT NULL,
-  `INTERMITANTE` varchar(256) NOT NULL,
-  `DEGRADE` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `DOC_D`
---
-
-CREATE TABLE `DOC_D` (
-  `IDDOCD` int(11) NOT NULL,
-  `URLDOCD` int(11) NOT NULL,
-  `NAMEDOCD` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `DOC_U`
---
-
-CREATE TABLE `DOC_U` (
-  `IDDOCU` int(11) NOT NULL,
-  `URLDOCU` int(11) NOT NULL,
-  `NAMEDOCU` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `DTOS`
---
-
-CREATE TABLE `DTOS` (
-  `IDSETUP` int(11) NOT NULL,
-  `IDDEFAI` int(11) NOT NULL,
-  `IDDOCU` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `SETUP`
---
-
-CREATE TABLE `SETUP` (
-  `IDSETUP` int(11) NOT NULL,
-  `NUMERO` tinyint(1) NOT NULL DEFAULT '0',
-  `FONCTION` tinyint(1) NOT NULL DEFAULT '0',
-  `EXIFONCTIONNEL` tinyint(1) DEFAULT '0',
-  `ORGANE` tinyint(1) DEFAULT '0',
-  `MODEDEFAI` tinyint(1) NOT NULL DEFAULT '0',
-  `CAUSELOCAL` tinyint(1) NOT NULL DEFAULT '0',
-  `EFFETLOCAL` tinyint(1) NOT NULL DEFAULT '0',
-  `BESOIN` tinyint(1) DEFAULT '0',
-  `EXIBESOIN` tinyint(1) DEFAULT '0',
-  `NVLLIGNE` tinyint(1) NOT NULL DEFAULT '0',
-  `AGARDER` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -106,51 +32,18 @@ CREATE TABLE `USER` (
   `PRENOM` varchar(254) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `UTOD`
+-- Contenu de la table `USER`
 --
 
-CREATE TABLE `UTOD` (
-  `IDUSER` int(11) NOT NULL,
-  `IDDOCU` int(11) NOT NULL,
-  `IDDOCD` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `USER` (`IDUSER`, `PERMISSION`, `URLUSER`, `USERNAME`, `EMAIL`, `PASSWORD`, `SOCIETE`, `NOM`, `PRENOM`) VALUES
+(1, 1, '', 'adepelley', 'aloise.depelley@insa-cvl.fr', 'c5992ae9801d756f8cba5a61589771bfd06046f6', 'INSA', 'Depelley', 'Aloïse'),
+(2, 1, '', 'jiroco', 'jean.savoldeli@insa-cvl.fr', '51f8b1fa9b424745378826727452997ee2a7c3d7', 'INSA', 'Savoldeli', 'Jean'),
+(3, 0, '', 'dneyron', 'david.neyron@insa-cvl.fr', 'aa743a0aaec8f7d7a1f01442503957f4d7a2d634', 'INSA', 'Neyron', 'David');
 
 --
 -- Index pour les tables exportées
 --
-
---
--- Index pour la table `DEFAILLANCE`
---
-ALTER TABLE `DEFAILLANCE`
-  ADD PRIMARY KEY (`IDDEFAI`);
-
---
--- Index pour la table `DOC_D`
---
-ALTER TABLE `DOC_D`
-  ADD PRIMARY KEY (`IDDOCD`);
-
---
--- Index pour la table `DOC_U`
---
-ALTER TABLE `DOC_U`
-  ADD PRIMARY KEY (`IDDOCU`);
-
---
--- Index pour la table `DTOS`
---
-ALTER TABLE `DTOS`
-  ADD PRIMARY KEY (`IDSETUP`);
-
---
--- Index pour la table `SETUP`
---
-ALTER TABLE `SETUP`
-  ADD PRIMARY KEY (`IDSETUP`);
 
 --
 -- Index pour la table `USER`
@@ -159,37 +52,11 @@ ALTER TABLE `USER`
   ADD PRIMARY KEY (`IDUSER`);
 
 --
--- Index pour la table `UTOD`
---
-ALTER TABLE `UTOD`
-  ADD PRIMARY KEY (`IDUSER`);
-
---
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT pour la table `DEFAILLANCE`
---
-ALTER TABLE `DEFAILLANCE`
-  MODIFY `IDDEFAI` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `DOC_D`
---
-ALTER TABLE `DOC_D`
-  MODIFY `IDDOCD` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `DOC_U`
---
-ALTER TABLE `DOC_U`
-  MODIFY `IDDOCU` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `SETUP`
---
-ALTER TABLE `SETUP`
-  MODIFY `IDSETUP` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT pour la table `USER`
 --
 ALTER TABLE `USER`
-  MODIFY `IDUSER` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IDUSER` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
