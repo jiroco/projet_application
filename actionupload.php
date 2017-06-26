443d885a5db41c74ff732932baa27c738ff06789
davidneyron <david.neyron@gmail.com>
2017-06-26 10:04:17 +0200

    Ajout log Upload

--- actionupload.php
+++ actionupload.php
@@ -1,8 +1,8 @@
 <?php
-header('charset=iso-8859-1');
-if ($_FILES['File']['error'] > 0) $erreur = "Erreur lors du transfert";
-$nom = md5(uniqid(rand(), true));
-$dest="MeDISIS_APP\Entrées\.".$nom.".xml";
-$resultat = move_uploaded_file($_FILES['File']['tmp_name'],$dest);
-//if ($resultat) echo "<meta http-equiv='refresh' content='0; URL=reglage_defaillance.php'>";;
+    header('charset=iso-8859-1');
+    if ($_FILES['File']['error'] > 0) $erreur = "Erreur lors du transfert";
+    $nom = md5(uniqid(rand(), true));
+    $dest="MeDISIS_APP\Entrées\.".$nom.".xml";
+    $resultat = move_uploaded_file($_FILES['File']['tmp_name'],$dest);
+    //if ($resultat) echo "<meta http-equiv='refresh' content='0; URL=reglage_defaillance.php'>";
 ?>
