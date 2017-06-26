5b58bd871d067934037b4bf434096b9f5a957052
davidneyron <david.neyron@gmail.com>
2017-06-26 14:12:33 +0200

    Erreur volontaire supprimee

--- accueil_connecte.php
+++ accueil_connecte.php
@@ -108,11 +108,5 @@ if ((!isset($_SESSION['IDUSER'])) || (empty($_SESSION['IDUSER']))){
               });
           });
         </script>
-
-        <br />
-<font size='1'><table class='xdebug-error xe-fatal-error' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Fatal error: Cannot use [] for reading in C:\wamp64\www\projet_application\include\log.php on line <i>22</i></th></tr>
-</table></font>
-
 	</body>
 </html>
