8616bb6becb6f821cb73b0ea938fb0e20ec22ffb
jiroco <jean.savoldelli@insa-cvl.fr>
2017-06-26 16:12:49 +0200

    amelioration de la page excel

--- include/excel.php
+++ include/excel.php
@@ -1,24 +1,96 @@
 <?php
 include("connexiondb.php");
+session_start();
 $req=$DBcon->prepare("SELECT * FROM setup");
 $check=$req->execute();
+
+
+//traiter le $_POST des titres de la premiere ligne
+if (isset($_POST["firstset"]) && !empty($_POST["firstset"])){
+	$first=$_POST["firstset"];
+}
+else{
+	$first="Numero";
+}
+if (isset($_POST["secondset"]) && !empty($_POST["secondset"])){
+	$second=$_POST["secondset"];
+}
+else{
+	$second="Fonction";
+}
+if (isset($_POST["thirdset"]) && !empty($_POST["thirdset"])){
+	$third=$_POST["thirdset"];
+}
+else{
+	$third="Exigence fonctionnelle";
+}
+if (isset($_POST["fourthset"]) && !empty($_POST["fourthset"])){
+	$fourth=$_POST["fourthset"];
+}
+else{
+	$fourth="Organe";
+}
+if (isset($_POST["fiveset"]) && !empty($_POST["fiveset"])){
+	$five=$_POST["fiveset"];
+}
+else{
+	$five="Mode de défaillance";
+}
+if (isset($_POST["sixset"]) && !empty($_POST["sixset"])){
+	$six=$_POST["sixset"];
+}
+else{
+	$six="Cause locale";
+}
+if (isset($_POST["sevenset"]) && !empty($_POST["sevenset"])){
+	$seven=$_POST["sevenset"];
+}
+else{
+	$seven="Effet locale";
+}
+if (isset($_POST["eightset"]) && !empty($_POST["eightset"])){
+	$eight=$_POST["eightset"];
+}
+else{
+	$eight="Besoin";
+}
+if (isset($_POST["nineset"]) && !empty($_POST["nineset"])){
+	$nine=$_POST["nineset"];
+}
+else{
+	$nine="Exigence besoin";
+}
+if (isset($_POST["tenset"]) && !empty($_POST["tenset"])){
+	$ten=$_POST["tenset"];
+}
+else{
+	$ten="Nouvelle ligne ?";
+}
+if (isset($_POST["elevenset"]) && !empty($_POST["elevenset"])){
+	$eleven=$_POST["elevenset"];
+}
+else{
+	$eleven="A garder ?";
+}
+
+
 
 if($check){
 	$output = '
 		<table class="table" bordered="1">
 		<tr>
 			<th></th>
-			<th>'.$_POST["firstset"].'</th>
-			<th>'.$_POST["secondset"].'</th>
-			<th>'.$_POST["thirdset"].'</th>
-			<th>'.$_POST["fourthset"].'</th>
-			<th>'.$_POST["fiveset"].'</th>
-			<th>'.$_POST["sixset"].'</th>
-			<th>'.$_POST["sevenset"].'</th>
-			<th>'.$_POST["eightset"].'</th>
-			<th>'.$_POST["nineset"].'</th>
-			<th>'.$_POST["tenset"].'</th>
-			<th>'.$_POST["elevenset"].'</th>
+			<th>'.$first.'</th>
+			<th>'.$second.'</th>
+			<th>'.$third.'</th>
+			<th>'.$fourth.'</th>
+			<th>'.$five.'</th>
+			<th>'.$six.'</th>
+			<th>'.$seven.'</th>
+			<th>'.$eight.'</th>
+			<th>'.$nine.'</th>
+			<th>'.$ten.'</th>
+			<th>'.$eleven.'</th>
 		</tr>
 		';
 
@@ -52,7 +124,7 @@ $req->closeCursor();
  
 //header("Content-Type: application/xls");
 //header("Content-disposition: inline; filename=/data/download.xls");
-$file= fopen("../data/reglage_setup.xls","w+");
+$file= fopen("../data/".$_SESSION['USERNAME']."/reglage_setup.xls","w+");
 fputcsv($file,array($output));
 
 ?>
