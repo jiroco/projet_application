792d5696c3c52d80489618811105cf70de0db9e3
ClaireHmt <claire.hamonet2013@laposte.net>
2017-06-26 12:48:06 +0200

    EXECUTION SCRIPTS REUSSIS 	new file:   MeDISIS_APP2/script.php 	modified:   actionupload.php 	modified:   register.php

--- actionupload.php
+++ actionupload.php
@@ -1,8 +1,9 @@
 <?php
 header('charset=iso-8859-1');
 if ($_FILES['File']['error'] > 0) $erreur = "Erreur lors du transfert";
-$nom = md5(uniqid(rand(), true));
-$dest="MeDISIS_APP2\InputFile\.".$nom.".xml";
+//$nom = md5(uniqid(rand(), true));
+$nom=$_FILES['File']['name'];
+$dest="MeDISIS_APP2\\InputFile\\".$nom;
 $resultat = move_uploaded_file($_FILES['File']['tmp_name'],$dest);
-if ($resultat) echo "<meta http-equiv='refresh' content='0; URL=reglage_defaillance.php'>";
+if ($resultat) echo "<meta http-equiv='refresh' content='0; URL=MeDISIS_APP2/script.php'>";;
 ?>
