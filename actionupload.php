2abca463da156d9f6c309ad8ff1c5fc04a76752a
ClaireHmt <claire.hamonet2013@laposte.net>
2017-06-27 15:32:27 +0200

    modified:   actionupload.php

--- actionupload.php
+++ actionupload.php
@@ -3,6 +3,7 @@
     session_start();
     include("include/connexiondb.php");
     header('charset=iso-8859-1');
+    $valide=0;
     if ($_FILES['File']['error'] > 0){
         $erreur = "Erreur lors du transfert";
     }
@@ -23,24 +24,27 @@
             $dest="./data/".$_SESSION["USERNAME"]."/upload/".$nom.".xml";
             $resultat = move_uploaded_file($file['tmp_name'],$dest);
             $req=$DBcon->prepare('INSERT INTO `DOC_U`(`IDDOCU`, `URLDOCU`, `NAMEDOCU`) VALUES (NULL,?,?)');
-            $req->bindValue(1,$dest,PDO::PARAM_STR);
-            $req->bindValue(2,$nom.".xml",PDO::PARAM_STR);
-            $req->execute();
-            $req=$DBcon->prepare('SELECT * FROM DOC_U where NAMEDOCU=?');
-            $req->bindValue(1,$nom.".xml",PDO::PARAM_STR);
-            $req->execute();
-            $donnee=$req->fetch();
-            $IDdocu=$donnee["IDDOCU"];
-            $req=$DBcon->prepare('INSERT INTO `UTOD`(`INDEXKEY`, `IDUSER`, `IDDOCU`, `IDDOCD`) VALUES (NULL,?,?,1)');
-            $req->bindValue(1,$_SESSION["IDUSER"],PDO::PARAM_STR);
-            $req->bindValue(2,$IDdocu,PDO::PARAM_STR);
-            $req->execute();
-            if ($resultat){
-                echo "<meta http-equiv='refresh' content='0; URL=reglage_defaillance.php'>";
-            }
-        }
-        else{
-           echo "</br><div class='container col-md-4 col-md-offset-4'><div class='alert alert-danger' style='text-align: center;'> <strong>Attention !</strong> Extension de fichiers incorrectes!. </div></div>";;
-        }
-    }
+$req->bindValue(1,$dest,PDO::PARAM_STR);
+$req->bindValue(2,$nom.".xml",PDO::PARAM_STR);
+$req->execute();
+$req=$DBcon->prepare('SELECT * FROM DOC_U where NAMEDOCU=?');
+$req->bindValue(1,$nom.".xml",PDO::PARAM_STR);
+$req->execute();
+$donnee=$req->fetch();
+$IDdocu=$donnee["IDDOCU"];
+$req=$DBcon->prepare('INSERT INTO `UTOD`(`INDEXKEY`, `IDUSER`, `IDDOCU`, `IDDOCD`) VALUES (NULL,?,?,1)');
+$req->bindValue(1,$_SESSION["IDUSER"],PDO::PARAM_STR);
+$req->bindValue(2,$IDdocu,PDO::PARAM_STR);
+$req->execute();
+if ($resultat){
+    echo "<meta http-equiv='refresh' content='0; URL=reglage_defaillance.php'>";
+}
+}
+else{
+$valide+=1;
+}
+if (isset($_POST['submit']) && $valide>0 ) {
+echo "</br><div class='container col-md-4 col-md-offset-4'><div class='alert alert-danger' style='text-align: center;'> <strong>Attention !</strong> Extension de fichiers incorrectes! </div></div><br/><br/><br/><br/>";;
+}
+}
 ?>
