1efe35e14859af16616cdb0d1707ba913656a068
davidneyron <david.neyron@gmail.com>
2017-06-27 14:12:36 +0200

    Poudre de perlinpinpin

--- include/delete.php
+++ include/delete.php
@@ -3,10 +3,20 @@
     include("connexiondb.php");
 
     $namedocu = $_POST["NAMEDOCU"];
+    $username = $_POST["USERNAME"];
 
-    $req = $DBcon->prepare('DELETE FROM `doc_u` WHERE `doc_u`.`NAMEDOCU` = ?');
+    $pathDir = "../data/" . $username . "/upload";
+
+    $pathFile = $pathDir . "/" . $namedocu;
+
+    $req = $DBcon->prepare('DELETE FROM `DOC_U` WHERE `DOC_U`.`NAMEDOCU` = ?');
     $req->bindValue(1,$namedocu,PDO::PARAM_INT);
     $req->execute();
+
+    $opening = opendir($pathDir);
+    unlink($pathFile);
+    closedir($opening);
+
 
     echo $namedocu;
 
